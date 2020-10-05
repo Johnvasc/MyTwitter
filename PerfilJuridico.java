236e8e3a691c8994a1659fdf14f3a81c8f8db7da
John Vasconcelos <39773960+Johnvasc@users.noreply.github.com>
2020-10-05 14:33:42 -0300

    Create PerfilJuridico.java

---
+++ PerfilJuridico.java
@@ -0,0 +1,12 @@
+public class PessoaJuridica extends Perfil{
+    private long cnpj;
+    
+//=========Getrs e Setrs===================
+
+    public long getCnpj(){
+        return this.cnpj;
+    }
+    public void setCnpj(long n){
+        this.cnpj = n;
+    }
+}
