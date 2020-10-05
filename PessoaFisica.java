a9023313603b32882b95c08258d6d183a04aa7f7
John Vasconcelos <39773960+Johnvasc@users.noreply.github.com>
2020-10-05 14:30:29 -0300

    Create PessoaFisica.java

---
+++ PessoaFisica.java
@@ -0,0 +1,9 @@
+public class PessoaFisica extends Perfil(){
+    private long cpf;
+    public long getCpf(){
+        return this.cpf;
+    }
+    public void setCpf(long n){
+        this.cpf = n;
+    }
+}
