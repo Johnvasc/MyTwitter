ab7ab21585c560dc445d814faf8fc1a54289f2f1
John Vasconcelos <39773960+Johnvasc@users.noreply.github.com>
2020-10-07 18:08:08 -0300

    Update IRepositorioUsuario.java

--- IRepositorioUsuario.java
+++ IRepositorioUsuario.java
@@ -1,5 +1,5 @@
 public interface IRepositorioUsuario{
     public void cadastrar(Perfil usuario);
     public void atualizar(Perfil usuario);
-    public Pefil buscar(String usuario); 
+    public Perfil buscar(String usuario); 
 }
